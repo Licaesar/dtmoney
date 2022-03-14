505f61b257df1ae3ed7217b162de0824812d6703
Licaesar <licaesar@pop-os.localdomain>
2022-03-14 15:56:00 -0400

    add header

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,15 +4,15 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --background: #f0f2f5;
         --red: #e52e4d;
-        --blue: #5429CC
+        --blue: #5429CC;
 
-        --blue-light: #6933FF
+        --blue-light: #6933FF;
 
-        --text-title: #363F5F
-        --text-body: #969CB3
+        --text-title: #363F5F;
+        --text-body: #969CB3;
 
-        --background: #F0F2F5
-        --shape: #FFFFFF
+        --background: #F0F2F5;
+        --shape: #FFFFFF;
     }
 
     * {
