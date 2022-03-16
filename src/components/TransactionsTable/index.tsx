180b2126cd08408551b6f5e5cdc847195d651cd5
Henrique Anjos da Costa <rique.pvh@outlook.com>
2022-03-16 11:23:15 -0400

    finish components

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,31 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <th>Titulo</th>
+                    <th>Valor</th>
+                    <th>Categoria</th>
+                    <th>Data</th>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$1.100</td>
+                        <td>Casa</td>
+                        <td>10/05/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    );
+}
